6403101cd10e1122b52602da07f1829a329076e3
Mo Firouz <mo@heroiclabs.com>
2017-05-18 00:00:45 +0100

    Setup lua_path correctly

--- server/script_runtime.go
+++ server/script_runtime.go
@@ -1,12 +1,8 @@
 package server
 
 import (
-	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
-
-	"fmt"
 
 	"github.com/yuin/gopher-lua"
 	"go.uber.org/zap"
@@ -15,15 +11,16 @@ import (
 
 type ScriptRuntime struct {
 	logger        *zap.Logger
-	modules       map[string]string //name to content
+	luaPath       string
 	luaLibs       map[string]lua.LGFunction
+	modules       []string
 	snapshotState *lua.LState
 }
 
 func NewScriptRuntime(logger *zap.Logger, multiLogger *zap.Logger, datadir string) *ScriptRuntime {
 	r := &ScriptRuntime{
 		logger:  logger,
-		modules: make(map[string]string),
+		modules: make([]string, 0),
 		luaLibs: map[string]lua.LGFunction{
 			lua.LoadLibName:      lua.OpenPackage,
 			lua.BaseLibName:      lua.OpenBase,
@@ -40,59 +37,43 @@ func NewScriptRuntime(logger *zap.Logger, multiLogger *zap.Logger, datadir strin
 }
 
 func (r *ScriptRuntime) loadRuntimeModules(multiLogger *zap.Logger, datadir string) {
-	// Make module dir if not exists
-	modulesPath := filepath.Join(datadir, "modules")
-	os.MkdirAll(modulesPath, os.ModePerm)
+	r.luaPath = filepath.Join(datadir, "modules")
+	os.MkdirAll(r.luaPath, os.ModePerm)
 
-	r.logger.Info("Loading modules", zap.String("path", modulesPath))
-
-	// Accumulate all Lua modules paths
-	modules := make([]string, 0)
-	err := filepath.Walk(modulesPath, func(path string, f os.FileInfo, err error) error {
+	r.logger.Info("Loading modules", zap.String("path", r.luaPath))
+	err := filepath.Walk(r.luaPath, func(path string, f os.FileInfo, err error) error {
 		if err != nil {
 			r.logger.Warn("Could not read module - skipping", zap.Error(err))
 		} else if !f.IsDir() {
-			content, err := ioutil.ReadFile(path)
-			if err != nil {
-				r.logger.Warn("Could not read module - skipping", zap.Error(err))
-			} else {
-				modules = append(modules, path)
-				modulePath := strings.TrimSuffix(path, filepath.Ext(path))
-				r.modules[modulePath] = string(content)
-			}
+			r.modules = append(r.modules, path)
 		}
 		return nil
 	})
 	if err != nil {
 		r.logger.Error("Failed to load modules", zap.Error(err))
 	}
+	multiLogger.Info("Evaluating modules", zap.Int("count", len(r.modules)), zap.Strings("modules", r.modules))
 
-	multiLogger.Info("Evaluating modules", zap.Int("count", len(r.modules)), zap.Strings("modules", modules))
-
-	l := r.NewState(r.modules)
+	// first time create a blank state
+	// and evaluate all lua code for all register_XXX calls
+	// then discard, and keep reference in memory for later
+	l := r.NewState()
 	defer l.Close()
-	for name, mod := range r.modules {
-
-		// checked _LOADED to see if its already loaded or not
-
-		err := l.DoString(mod)
-		if err != nil {
-			r.logger.Error("Failed to evaluate module", zap.String("name", name), zap.Error(err))
-			delete(r.modules, name)
+	failedModules := 0
+	for _, mod := range r.modules {
+		if err = l.DoFile(mod); err != nil {
+			failedModules++
+			r.logger.Error("Failed to evaluate module", zap.String("path", mod), zap.Error(err))
 		}
 	}
 
-	multiLogger.Info("Loaded modules", zap.Int("count", len(r.modules)))
+	multiLogger.Info("Loaded modules", zap.Int("count", len(r.modules)-failedModules))
 
-	r.snapshotState = r.NewState(r.modules)
+	// final state to keep in memory
+	r.snapshotState = r.NewState()
 }
 
-func (r *ScriptRuntime) NewStateThread() (*lua.LState, context.CancelFunc) {
-	//TODO(mo) use context to limit lua processing time
-	return r.snapshotState.NewThread()
-}
-
-func (r *ScriptRuntime) NewState(modules map[string]string) *lua.LState {
+func (r *ScriptRuntime) NewState() *lua.LState {
 	l := lua.NewState(lua.Options{
 		CallStackSize:       1024,
 		RegistrySize:        1024,
@@ -100,60 +81,33 @@ func (r *ScriptRuntime) NewState(modules map[string]string) *lua.LState {
 		IncludeGoStackTrace: true,
 	})
 
+	// override before Package library is invoked.
+	lua.LuaLDir = r.luaPath
+	lua.LuaPathDefault = lua.LuaLDir + "/?.lua;" + lua.LuaLDir + "/?/init.lua"
+	os.Setenv(lua.LuaPath, lua.LuaPathDefault)
+
 	for name, lib := range r.luaLibs {
 		l.Push(l.NewFunction(lib))
 		l.Push(lua.LString(name))
 		l.Call(1, 0)
 	}
-
-	env := l.Get(lua.EnvironIndex)
-	l.SetField(env, "require", l.NewFunction(func(ls *lua.LState) int {
-		name := l.CheckString(1)
-
-		loaded := l.GetField(l.Get(lua.RegistryIndex), "_LOADED")
-		lv := l.GetField(loaded, name)
-		if lua.LVAsBool(lv) {
-			fmt.Println("found in _LOADED")
-			return 1
-		}
-
-		if mod, ok := modules[name]; ok {
-			fmt.Println("loading again...")
-			modasfunc, err := l.LoadString(mod)
-			if err != nil {
-				l.Error(lua.LString("require failed to load module "+name), 1)
-				r.logger.Error("require failed to load module", zap.String("path", name), zap.Error(err))
-				return 0
-			}
-
-			l.Push(modasfunc)
-			l.Push(lua.LString(name))
-			l.Call(1, 1)
-			l.SetField(loaded, name, lua.LTrue)
-
-			//TODO(mo) is this the best way to load a module?
-			//preload := l.GetField(l.GetField(env, "package"), "preload")
-			//l.SetField(preload, name, lf)
-
-			//l.SetField(loaded, name, modasfunc)
-			//l.Push(modasfunc)
-			return 1
-		}
-
-		l.Error(lua.LString("require failed to find module "+name), 1)
-		r.logger.Warn("require failed to find module", zap.String("path", name))
-		return 0
-	}))
-
-	//preload := l.GetField(l.GetField(l.Get(lua.EnvironIndex), "package"), "preload")
-	//for name, module := range r.modules {
-	//	mod, err := l.LoadString(module)
-	//	if err != nil {
-	//		r.logger.Error("Failed to evaluate module", zap.String("name", name), zap.Error(err))
-	//	} else {
-	//		l.SetField(preload, name, mod)
-	//	}
-	//}
-
 	return l
+}
+
+func (r *ScriptRuntime) PreloadModules(l *lua.LState, modules map[string]string) {
+	preload := l.GetField(l.GetField(l.Get(lua.EnvironIndex), "package"), "preload")
+
+	for name, mod := range modules {
+		f, err := l.LoadString(mod)
+		if err != nil {
+			r.logger.Error("Could not preload module", zap.String("name", name), zap.Error(err))
+		} else {
+			l.SetField(preload, name, f)
+		}
+	}
+}
+
+func (r *ScriptRuntime) NewStateThread() (*lua.LState, context.CancelFunc) {
+	//TODO(mo) use context to limit lua processing time
+	return r.snapshotState.NewThread()
 }
--- tests/script_runtime_test.go
+++ tests/script_runtime_test.go
@@ -1,15 +1,21 @@
 package tests
 
 import (
+	"io/ioutil"
 	"nakama/server"
+	"os"
 	"testing"
+
+	"path/filepath"
 
 	"go.uber.org/zap"
 )
 
+var DATA_PATH = "/tmp/nakama/data/"
+
 func newRuntime() *server.ScriptRuntime {
 	logger, _ := zap.NewDevelopment(zap.AddStacktrace(zap.ErrorLevel))
-	return server.NewScriptRuntime(logger, logger, "data")
+	return server.NewScriptRuntime(logger, logger, DATA_PATH)
 }
 
 func TestRuntimeSampleScript(t *testing.T) {
@@ -20,8 +26,7 @@ func TestRuntimeSampleScript(t *testing.T) {
 local example = "an example string"
 for i in string.gmatch(example, "%S+") do
    print(i)
-end
-	`)
+end`)
 
 	if err != nil {
 		t.Error(err)
@@ -39,8 +44,7 @@ function file_exists(path)
   if file then file:close() end
   return file ~= nil
 end
-file_exists "./"
-	`)
+file_exists "./"`)
 
 	if err == nil {
 		t.Error("Successfully accessed IO package")
@@ -49,9 +53,48 @@ file_exists "./"
 	}
 }
 
-func TestRuntimeRequire(t *testing.T) {
+func TestRuntimeRequireFile(t *testing.T) {
 	r := newRuntime()
-	l := r.NewState(map[string]string{
+
+	statsMod := []byte(`
+stats={}
+print("...loading file...")
+-- Get the mean value of a table
+function stats.mean( t )
+  local sum = 0
+  local count= 0
+
+  for k,v in pairs(t) do
+    if type(v) == 'number' then
+      sum = sum + v
+      count = count + 1
+    end
+  end
+
+  return (sum / count)
+end
+
+return stats`)
+	ioutil.WriteFile(filepath.Join(DATA_PATH, "/modules/stats.lua"), statsMod, 0644)
+
+	l := r.NewState()
+	err := l.DoString(`
+local stats = require("stats")
+t = {[1]=5, [2]=7, [3]=8, [4]='Something else.'}
+print(stats.mean(t))
+		`)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	os.RemoveAll(DATA_PATH)
+}
+
+func TestRuntimeRequirePreload(t *testing.T) {
+	r := newRuntime()
+	l := r.NewState()
+	r.PreloadModules(l, map[string]string{
 		"stats": `
 stats={}
 
@@ -70,30 +113,10 @@ function stats.mean( t )
   return (sum / count)
 end
 
-return stats
-	`,
-	})
+return stats`})
 	defer l.Close()
 
-	//
-	//preload := l.GetField(l.GetField(l.Get(lua.EnvironIndex), "package"), "preload")
-	//l.SetField(preload, "stats", statsModule)
-
-	//if e != nil {
-	//	t.Error(e)
-	//}
-
 	err := l.DoString(`
-local stats = require("stats")
-t = {[1]=5, [2]=7, [3]=8, [4]='Something else.'}
-print(stats.mean(t))
-		`)
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	err = l.DoString(`
 local stats = require("stats")
 t = {[1]=5, [2]=7, [3]=8, [4]='Something else.'}
 print(stats.mean(t))
